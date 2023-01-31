9560690de098988fc5f3197a1c84eb51161c704c
pdh <qwer5383@naver.com>
2023-01-31 21:14:34 +0900

    노드 서버에 비디오 저장하기

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import  { Typography, Button, Form, message, Input, Icon } from 'antd';
 import DropZone from 'react-dropzone';
-import ResizableTextArea from 'antd/lib/input/ResizableTextArea';
+
+import Axios from 'axios'
 
 const  { TextArea } = Input;
 const { Title } = Typography;
@@ -40,6 +41,23 @@ function VideoUploadPage() {
         setCategory(e.currentTarget.value)
     }
 
+    const onDrop = (files) => {
+        let formData = new FormData;
+        const config = {
+            header: {'content-type' : 'multipart/form-data'}
+        }
+        formData.append("file", files[0])
+
+        Axios.post('/api/video/uploadfiles', formData, config)
+            .then(response => {
+                if(response.data.success) {
+                    console.log(response.data)
+                } else {
+                    alert('비디오 업로 실패')
+                }
+            })
+    }
+
     return (
         <div style={{ maxWidth:'700px', margin:'2rem auto' }}>
             <div style={{ textAlign:'center', marginBottom:'2rem' }}>
@@ -50,9 +68,10 @@ function VideoUploadPage() {
                 <div style={{ display:'flex', justifyContent:'space-between' }}>
                     {/* Drop Zone  */}
                     <DropZone
-                        onDrop
-                        multiple
-                        maxSize>
+                        onDrop={onDrop}
+                        multiple={false}
+                        maxSize={10000000000}
+                        >
                         {({ getRootProps, getInputProps }) =>(
                             <div style={{ width:'300px', height:'240px', border:'1px solid lightgray', display:'flex',
                             alignItems:'center', justifyContent:'center'}} {...getRootProps() }> 

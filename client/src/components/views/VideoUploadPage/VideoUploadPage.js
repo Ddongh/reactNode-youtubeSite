abcbdd281470c206a565cef2c07d9302eb909a6e
pdh <qwer5383@naver.com>
2023-02-04 20:55:05 +0900

    ffmpeg로 비디오 썸네일 생성하기& gitIgnore수정

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -24,6 +24,9 @@ function VideoUploadPage() {
     const [Description, setDescription] = useState("")
     const [Private, setPrivate] = useState(0)
     const [Category, setCategory] = useState("Film & Animation")
+    const [FilePath, setFilePath] = useState("")
+    const [Duration, setDuration] = useState("")
+    const [ThumbnailPath, setThumbnailPath] = useState("")
 
     const onTitleChange = (e) => {
         setVidioTitle(e.currentTarget.value)
@@ -52,6 +55,25 @@ function VideoUploadPage() {
             .then(response => {
                 if(response.data.success) {
                     console.log(response.data)
+
+                    let variable = {
+                        url: response.data.url,
+                        fileName: response.data.fileName
+                    }
+
+                    setFilePath(response.data.url)
+
+                    Axios.post('/api/video/thumbnail', variable)
+                    .then(response => {
+                        if(response.data.success) {
+                            
+                            setDuration(response.data.fileDuration)
+                            setThumbnailPath(response.data.url)
+
+                        } else {
+                            alert('썸네일 생성에 실패했습니다.')
+                        }
+                    })
                 } else {
                     alert('비디오 업로 실패')
                 }
@@ -84,9 +106,15 @@ function VideoUploadPage() {
                     </DropZone>
 
                     {/* Thumbnail */}
-                    <div>
-                        <img src alt />
-                    </div>
+
+                    {ThumbnailPath &&
+                        <div>
+                        <img src={`http://localhost:5001/${ThumbnailPath}`} 
+                            alt={"thumbnail"} 
+                        />
+                        </div>
+                    }
+                    
                 </div>
                 <br />
                 <br />

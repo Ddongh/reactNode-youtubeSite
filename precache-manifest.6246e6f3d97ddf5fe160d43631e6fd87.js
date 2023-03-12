7ff98d79c13e6d5ef338ee52bd994fa088b1bf09
Ddongh <qwer5383@naver.com>
2023-03-13 01:30:02 +0900

    Updates

--- precache-manifest.6246e6f3d97ddf5fe160d43631e6fd87.js
+++ precache-manifest.6246e6f3d97ddf5fe160d43631e6fd87.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9f523713603f4ad2541ffa63b98f0728",
+    "revision": "29ad7d993515005278cf72ced4741796",
     "url": "/reactNode-youtubeSite/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactNode-youtubeSite/static/css/2.051905ae.chunk.css"
   },
   {
-    "revision": "59a66eba175cb0bec672",
+    "revision": "36092dcc7263b6f82e22",
     "url": "/reactNode-youtubeSite/static/css/main.b5d3c51a.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactNode-youtubeSite/static/js/2.582e8023.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "59a66eba175cb0bec672",
-    "url": "/reactNode-youtubeSite/static/js/main.80192e34.chunk.js"
+    "revision": "36092dcc7263b6f82e22",
+    "url": "/reactNode-youtubeSite/static/js/main.fe4c28bd.chunk.js"
   },
   {
     "revision": "d16f98bdfb29294c33b1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactNode-youtubeSite/precache-manifest.4f39e2db40450619fdfe42548161ce30.js"
+  "/reactNode-youtubeSite/precache-manifest.6246e6f3d97ddf5fe160d43631e6fd87.js"
 );
 
 self.addEventListener('message', (event) => {
